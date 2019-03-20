286089feebecf1dfa6cfef12060c68ee19b23f1e
s210276 Sylwia Mieszkowska <210276@edu.p.lodz.pl>
2019-03-20 13:12:07 +0100

    Implementacja i testy jednostkowe dla klasy FizzBuzz

--- src/main/java/pl/lodz/p/zzpj/testing/junit/tdd/FizzBuzz.java
+++ src/main/java/pl/lodz/p/zzpj/testing/junit/tdd/FizzBuzz.java
@@ -2,8 +2,27 @@ package pl.lodz.p.zzpj.testing.junit.tdd;
 
 public class FizzBuzz {
 
-    public String getFizzBuzzNumber(int number) {
-        //TODO: implement
-        return null;
+    public String getFizzBuzzNumber(int number) throws FizzBuzzException{
+        if(number < 0){
+        	throw new FizzBuzzException("Number must be positive.");
+		}
+
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		StringBuilder result = new StringBuilder();
+		if(number % 3 == 0){
+			result.append("Fizz");
+		}
+		if(number % 5 == 0){
+			result.append("Buzz");
+		}
+		if(number % 3 != 0 && number % 5 != 0){
+			result.append(Integer.toString(number));
+		}
+        return result.toString();
     }
 }
